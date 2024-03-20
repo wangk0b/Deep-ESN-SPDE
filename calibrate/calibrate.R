05a086a5a1e8947ff958d8db36cba74d829a1bc9
wangk0b <71711006+wangk0b@users.noreply.github.com>
2024-03-20 22:19:16 +0300

    Update calibrate.R

--- calibrate/calibrate.R
+++ calibrate/calibrate.R
@@ -1,6 +1,6 @@
 for(num in 10:10){
 #location wise
-holder=array(NA,c(10,8750,50160))
+holder=array(NA,c(10,8760,50160))
 #perlocation
 enum = (1+(num-1)*10):(num*10)
 count = 1 
@@ -17,14 +17,14 @@ for(i in enum){
 #set.seed(1)
 index = read.table("locselectR.txt",sep=",")[,1]
 index_all =setdiff(1:53333,index)
-hold_all = array(NA,dim=c(10,8750,53333))
+hold_all = array(NA,dim=c(10,8760,53333))
 #load("subsamples.rda")
 hold_all[,,index_all]=holder
 rm(holder)
 
 library(reticulate)
 np=import("numpy")
-ensembles=np$load('ESN_ensembles.npy')[enum,1:8750,,1]
+ensembles=np$load('ESN_ensembles.npy')[enum,1:8760,,1]
 hold_all[,,index]= ensembles
 rm(index_all)
 rm(index)
@@ -32,7 +32,7 @@ rm(ensembles)
 #load actual values
 library(ncdf4)
 nc_data = nc_open('wind_residual_all_locations.nc')
-wind_all=ncvar_get(nc_data,"wind_residual_all_locations")[26281:35030,]
+wind_all=ncvar_get(nc_data,"wind_residual_all_locations")[26281:35040,]
 print("dimensions of real data and predicted data")
 dim(wind_all)
 dim(hold_all)
