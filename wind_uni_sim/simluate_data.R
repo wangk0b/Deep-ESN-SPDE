e1147240f97f66a640bbf4abd4159019c4dc83d9
wangk0b <71711006+wangk0b@users.noreply.github.com>
2024-03-26 17:54:17 +0300

    Update simluate_data.R
    BI

--- wind_uni_sim/simluate_data.R
+++ wind_uni_sim/simluate_data.R
@@ -31,7 +31,11 @@ loc = np$load("loc_all.npy")
 #testing 
 
 N = 3200
-
+ind = 1:N
+index_b1 = ind[((loc[,1] > 0) & (loc[,1] < 0.5))& ((loc[,2] > 0.5) & (loc[,2] < 1))]
+index_b2 = ind[((loc[,1] > 0.5) & (loc[,1] < 1))& ((loc[,2] > 0.5) & (loc[,2] < 1))]
+index_b3 = ind[((loc[,1] > 0) & (loc[,1] < 0.5))& ((loc[,2] > 0) & (loc[,2] < 0.5))]
+index_b4 = ind[((loc[,1] > 0.5) & (loc[,1] < 1))& ((loc[,2] > 0) & (loc[,2] < 0.5))]
 #loc = expand.grid(x,y)
 d_mat = dist(loc,diag=T,upper=T)
 H_one = as.matrix(matern(1,0.03,0.5,d_mat))
@@ -41,15 +45,12 @@ H_two = as.matrix(matern(1,0.05,1,d_mat))
 diag(H_two) = 1
 ###################
 #sample from the two covariance matrices
-H_three = as.matrix(matern(1,0.03,1.5,d_mat))
-diag(H_three) = 1
-
+#######################################
+#t = 1000
 #sim_data = matrix(NA,nrow = t, ncol = N)
 
 L_one = t(chol(H_one))
 L_two = t(chol(H_two))
-L_three = t(chol(H_three))
-
 #########################################
 #N_one = L_one%*%rnorm(N)
 #N_two = L_two%*%rnorm(N)
@@ -72,24 +73,37 @@ L_three = t(chol(H_three))
 #}
 #pb = txtProgressBar(min = 1, max = N, initial = 1,style = 3) 
 
+#cl = makeCluster(30)
+#registerDoParallel(cl)
 
+
+#time_data = foreach(i = 1:N, .combine = cbind) %dopar% {
+# library(arfima)
+# set.seed(seed_option) 
+# sim_data = arfima.sim(1000, model = list(phi = c(0.3,-0.1), theta=c(0.1,-0.3), dfrac = .3, dint = 0))
+ #setTxtProgressBar(pb,i)
+# p = sim_data
+# p
+#}
 #close(pb)
+#stopCluster(cl)
 
 #pb.two = txtProgressBar(min = 1, max = t, initial = 1, style = 3)
+#set.seed(seed_option)
+N_one = L_one%*%rnorm(N)
+N_two = L_two%*%rnorm(N)
 
-  N_one = L_one%*%rnorm(N)
-  N_two = L_two%*%rnorm(N)
-  N_three = L_three%*%rnorm(N)
-  sim_data = 0.7*N_one + 0.15*N_two + 0.15*N_three
-  #setTxtProgressBar(pb.two,j)
+B1 = 0.7*N_one[index_b1] + 0.3*N_two[index_b1]
+B2 = 0.6*N_one[index_b2] + 0.4*N_two[index_b2]
+B3 = 0.9*N_one[index_b3] + 0.1*N_two[index_b3]
+B4 = 0.55*N_one[index_b4] + 0.45*N_two[index_b4]
+sim_data = c(B1,B2,B3,B4)
+#setTxtProgressBar(pb.two,j)
 ##########################################################################
 #print(sim_data)
+#np$save("time_data.npy",time_data)
 np$save("space_data.npy",sim_data)
 print("Done!")
-
-
-
-
 
 
 
